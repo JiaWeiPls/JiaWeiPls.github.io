74d86c8a79ed9d679c20e484e6a17fd40fc5aa82
Aldair Avelino <aldair03avelino@gmail.com>
2021-11-14 14:55:29 +0100

    Todas as variáveis criadas

--- assets/scripts/operations-calculator.js
+++ assets/scripts/operations-calculator.js
@@ -1,9 +1,33 @@
-let add;
-let subtract;
-let division;
-let multiplication;
+let display = document.querySelector(".display");
 
-console.log(add);
-console.log(subtract);
-console.log(division);
+let c = document.querySelector(".c");
+let signed = document.querySelector(".signed");
+let percent = document.querySelector(".percent");
+let decimal = document.querySelector(".decimal");
+
+
+let multiplication = document.querySelector(".times");
+let division = document.querySelector(".divide");
+let addition = document.querySelector(".plus");
+let subtraction = document.querySelector(".minus");
+let equals = document.querySelector(".equals");
+
+let zero = document.querySelector(".zero");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+
+
+
+
 console.log(multiplication);
+console.log(division);
+console.log(addition);
+console.log(subtract);
+console.log(equals);
