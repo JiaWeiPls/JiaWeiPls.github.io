47a4c180540513891d414d4329c4fae71008618a
JiaWeiPls <46553553+JiaWeiPls@users.noreply.github.com>
2023-02-28 07:16:39 +0000

    switched theme

--- assets/scripts/change-calculator-theme.js
+++ assets/scripts/change-calculator-theme.js
@@ -1,13 +1,15 @@
 /* Script para alterar o tema da Calculadora */
 
-const themeChanger = document.querySelector('.theme-switch input[type="checkbox"]');
+const themeChanger = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 
 function changeCalculatorTheme(data) {
-    if (data.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+  if (data.target.checked) {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
 }
 
-themeChanger.addEventListener('change', changeCalculatorTheme, false);
+themeChanger.addEventListener("change", changeCalculatorTheme, false);
